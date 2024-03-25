6c224527fc5d28ab63939159b6226043b6b68889
orwellian <orwellian225@gmail.com>
2024-03-25 23:43:09 +0200

    random optimisation to globalmem

--- src/globalmem.cu
+++ src/globalmem.cu
@@ -6,13 +6,13 @@
 
 __global__ void globalmem::convolve(Matrix *image, Matrix *kernel, Matrix *result) {
     size_t image_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (image_idx >= image->size)
+    if (threadIdx.x >= image->width)
         return;
 
-    result->data[image_idx] = 0.;
     size_t r = image_idx / image->width;
     size_t c = image_idx % image->width;
 
+    float sum = 0.;
     for (int32_t kernel_r = -(int32_t)(kernel->height / 2); kernel_r <= (int32_t)(kernel->height / 2); ++kernel_r) {
         for (int32_t kernel_c = -(int32_t)(kernel->width / 2); kernel_c <= (int32_t)(kernel->width / 2); ++kernel_c) {
 
@@ -23,8 +23,9 @@ __global__ void globalmem::convolve(Matrix *image, Matrix *kernel, Matrix *resul
             int32_t kernel_image_idx = kernel_image_r * image->height + kernel_image_c;
 
             if ( -1 < kernel_image_r && kernel_image_r < image->height && -1 < kernel_image_c && kernel_image_c < image->width )
-                result->data[image_idx] += image->data[kernel_image_idx] * kernel->data[kernel_idx];
+                sum += image->data[kernel_image_idx] * kernel->data[kernel_idx];
         }
     }
 
+    result->data[image_idx] = sum;
 }
